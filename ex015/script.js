6222908fae553c6309945b5a231b8e2041382c03
Wallyson Gomes <63168683+fuzaros@users.noreply.github.com>
2023-12-03 21:45:52 -0300

    bug do text align

--- ex015/script.js
+++ ex015/script.js
@@ -12,35 +12,44 @@ function verificar(){
         var gênero = ''
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
+
         if (fsex[0].checked){
             gênero = 'Homem'
 
             if(idade >=0 && idade < 10){
                 img.setAttribute('src', 'criançaHomem.png')
-
+                //criança
             }else if (idade < 21){
                 img.setAttribute('src', 'jovemHomem.png')
+                //jovem
             }else if (idade < 50){
                 img.setAttribute('src', 'adultoHomem.png')
+                //Adulto
             }else{
                 img.setAttribute('src', 'velhoHomem.png')
+                //idoso
             }
         }else if (fsex[1].checked) {
             gênero = 'Mulher'
 
             if(idade >=0 && idade < 10){
                 img.setAttribute('src', 'criancamulher.png')
+                //criança
             }else if (idade < 21){
                 img.setAttribute('src', 'adultoMulher.png')
+                //jovem
             }else if (idade < 50){
                 img.setAttribute('src', 'jovemMulher.png')
+                //Adulto
             }else{
                 img.setAttribute('src', 'velhoMulher.png')
+                //Idosa
             }
         }
-        res.style.textAlign = 'center'
+        
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
         res.appendChild(img)
+        
     }
 
 }
