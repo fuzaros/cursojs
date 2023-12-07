333f8fa98070f2f0ee7a42e9a1fb3ddb54c83a67
Wallyson Gomes <63168683+fuzaros@users.noreply.github.com>
2023-12-07 17:39:57 -0300

    novos

---
+++ aula13/ambiente.js
@@ -0,0 +1,27 @@
+var c = 1
+while (c <= 6 ){
+    console.log('Tudo bem?')
+    c++ // c = c + 1 explicada em operadores
+}
+
+var b = 1
+while (b <= 10) {
+    console.log(`Passo ${b}`)
+    b++
+}
+
+var a = 1
+do {
+    console.log(`Passo ${a}`)
+    a++
+}while (a <= 3)
+
+var c = 1 
+while (c <= 10){
+
+    c++
+}
+
+for(c = 1; c <= 10; c++){
+    console.log(`Hoje é ${c}`)
+}
---
+++ aula13/node_6a7ff7ae205f1.tmp.js
@@ -0,0 +1,10 @@
+var c = 1
+while (c <= 6 ){
+    console.log('Tudo bem?')
+    c++ // c = c + 1 explicada em operadores
+}
+
+var b = 1
+while (b <= 10) {
+    console.log(`Passo ${b}`)
+}
